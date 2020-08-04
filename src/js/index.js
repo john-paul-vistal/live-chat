cb23f81c8bf217db6ef40aedf2d4e24f9407b109
john-paul.vistal <johnpaulvistal@gmail.com>
2020-08-04 12:56:40 +0800

    Add redirect page

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-    window.onbeforeunload = function() {
-        return "You will get disconnected";
-    }
     var userName;
     var avatarImg;
 
@@ -13,8 +10,37 @@ $(document).ready(function() {
     $('#strtConvo').click(function() {
         userName = $('#userName').val();
         avatarImg = $('#choosenAvatar').attr('src');
-        $('#body').css('display', 'block');
-        $('#login').css('display', 'none');
+        if (userName != '') {
+            localStorage.setItem("userName", userName);
+            localStorage.setItem("avatarImg", avatarImg);
+            location.replace("chat-body.html")
+        } else {
+            Swal.fire({
+                position: 'top',
+                icon: 'error',
+                title: 'Oops...',
+                text: 'PLEASE PUT A USER NAME',
+            })
+        }
+    })
+    console.log(localStorage.getItem("avatarImg"))
+    $('#logout').click(function() {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You will get disconnected to your conversation!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, logout'
+        }).then((result) => {
+            if (result.value) {
+                localStorage.removeItem("userName");
+                localStorage.removeItem("avatarImg");
+                location.replace("index.html")
+            }
+        })
+
     })
 
     var client = mqtt.connect('wss://test.mosquitto.org:8081/mqtt')
@@ -30,7 +56,7 @@ $(document).ready(function() {
 
     client.on('message', function(topic, message) {
         let payload = JSON.parse(message);
-        if (payload.user == userName) {
+        if (payload.user == localStorage.getItem("userName")) {
             $('#chatBody').append('<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send messagepx">' + payload.message + '<span class="msg_time_send">' + payload.timestamp + '</span></div><div class="img_cont_msg"><img src="' + payload.avatar + '" class="rounded-circle user_img_msg"></div></div>')
         } else {
             $('#chatBody').append('<div class="d-flex justify-content-start mb-4"> <div class = "img_cont_msg" ><img src = "' + payload.avatar + '"class = "rounded-circle user_img_msg" ></div><div class = "msg_cotainer messagepx">' + payload.message + '<span class = "msg_time" >' + payload.timestamp + '</span></div></div>')
@@ -41,14 +67,17 @@ $(document).ready(function() {
 
     $('#sendBtn').click(function() {
         let msg = $('#message').val();
-        let timeStamp = new Date().toLocaleTimeString() + ', ' + new Date().toLocaleDateString()
-        let payload = {
-            user: userName,
-            avatar: avatarImg,
-            timestamp: timeStamp,
-            message: msg
+        if (msg != '') {
+            let timeStamp = new Date().toLocaleTimeString() + ', ' + new Date().toLocaleDateString()
+            let payload = {
+                user: localStorage.getItem("userName"),
+                avatar: localStorage.getItem("avatarImg"),
+                timestamp: timeStamp,
+                message: msg
+            }
+            client.publish('livechat/messages', JSON.stringify(payload));
+            $('#message').val('');
         }
-        client.publish('livechat/messages', JSON.stringify(payload));
     })
 
 }); //END CODE
